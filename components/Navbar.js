d2ef11841eb894546bbb152f4c5d06a6141e38c6
Laura Medina <lcmedina390@gmail.com>
2022-04-14 15:21:37 -0500

    Resume page is finished for web

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,6 @@ const Navbar = () => {
             <Image src="/icons/toggle.png" width={40} height={40}/>
             </button> */}
             <Link href="/"><a>Home</a></Link>
-            <Link href="/#contact" scroll={false}><a>Contact</a></Link>
             <Link href="/projects"><a>Projects</a></Link>
             <Link href="/resume"><a>Resume</a></Link>
         </nav>
